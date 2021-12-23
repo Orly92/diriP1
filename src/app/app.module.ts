ca8d43238b8d3ac4c32bd01723634384d733bbfd
Orlando Marques <realorlanditomadrid@gmail.com>
2021-12-23 17:22:01 +0100

    MOD: move page-title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { WinesComponent } from './wines/wines.component';
 import { ContactComponent } from './contact/contact.component';
-import { PageTitleComponent } from './page-title/page-title.component';
+import { PageTitleComponent } from './shared/page-title/page-title.component';
 
 @NgModule({
   declarations: [
