93d72534feba6922fdb4a4d462ce7cc66735f0bd
Orlando Marques <realorlanditomadrid@gmail.com>
2021-12-23 17:02:50 +0100

    ADD: router links to menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {WinesComponent} from "./wines/wines.component";
+import {ContactComponent} from "./contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:HomeComponent,
+  pathMatch:'full'
+},{
+  path:'listavinos',
+  component:WinesComponent,
+  pathMatch:'full'
+},{
+  path:'contacto',
+  component:ContactComponent,
+  pathMatch:'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
