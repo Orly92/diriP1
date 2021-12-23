7c9c226550a018d5c69463ce2cdab08f0700b359
Orlando Marques <realorlanditomadrid@gmail.com>
2021-12-23 20:11:11 +0100

    ADD: wineDetail component

---
+++ src/app/wine-detail/wine-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wine-detail',
+  templateUrl: './wine-detail.component.html',
+  styleUrls: ['./wine-detail.component.scss']
+})
+export class WineDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
